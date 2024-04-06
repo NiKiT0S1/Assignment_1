1a0c0f51a36cb3a335017aba2b0a41ab8a7799b7
NiKiT0S1 <bobilevnikita88@gmail.com>
2024-04-06 08:41:24 +0500

    finish 1st problem

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
+        int [] num = {5, 10, 1, 32, 3, 45};
+        int min = findMin(num.length, num);
+        System.out.println(min);
     }
 
     public static int findMin(int n, int[] arr) {
         int min = arr[0];
         for (int i = 1; i < n; i++) {
-
+            if (arr[i] < min) {
+                min = arr[i];
+            }
         }
+        return min;
     }
 }
